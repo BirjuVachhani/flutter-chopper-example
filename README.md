7647637030111fff7220358582e6ef3277577c15
BirjuVachhani <brvachhani@gmail.com>
2019-10-15 11:57:12 +0530

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_chopper_example
+
+This project demonstrates how chopper library can be used with Flutter to make network calls. Chopper is like Retrofit for Dart.
---
+++ android/app/src/main/kotlin/io/github/birjuvachhani/flutter_chopper_example/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.birjuvachhani.flutter_chopper_example
+
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
